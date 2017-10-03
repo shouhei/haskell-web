9975462e27403fc099a7e07663b2d34a6dcc8da3
山口将平 <shouhei.yamaguchi.be@gmail.com>
2017-10-04 00:21:13 +0900

    Add getRequestHeader test

--- test/Spec.hs
+++ test/Spec.hs
@@ -7,6 +7,18 @@ specGetServer = do
     it "standard" $
       getServer `shouldBe` "haskell-web"
 
+exampleRequestHeader :: String
+exampleRequestHeader = do "GET /hogehoge HTTP/1.0 \r\n\
+\hogehogehogehogehoge \r\n\
+\hugahugahugahuga \r\n\n"
+
+specGetRequestMethod :: Spec
+specGetRequestMethod = do
+  describe "getRequestMethod" $ do
+    it "standard" $
+      (getRequestMethod exampleRequestHeader) `shouldBe` "GET"
+
 main :: IO ()
 main = hspec $ do
   specGetServer
+  specGetRequestMethod
