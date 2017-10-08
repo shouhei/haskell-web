20495280d2dae775b53b72a9723692f22a5d5831
山口将平 <shouhei.yamaguchi.be@gmail.com>
2017-10-08 20:33:50 +0900

    Implement config file

--- app/Main.hs
+++ app/Main.hs
@@ -1,4 +1,4 @@
-{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE DataKinds, OverloadedStrings, DeriveGeneric #-}
 module Main where
 
 import Lib
@@ -6,14 +6,27 @@ import Options.Declarative
 import Control.Monad.Trans
 import Control.Exception
 import Network.Socket
+import Data.Aeson
+import GHC.Generics
+import qualified Data.ByteString.Lazy.Char8 as BS
+import Data.Maybe
+
+data Config = Config { allow_host :: String, port :: Int, document_root :: String } deriving (Show, Generic)
+instance FromJSON Config
+instance ToJSON Config
+
 
 main :: IO ()
 main = run_ server
 
-server :: Flag "p" '["port"] "INT" "specify server port" (Def "8080" Int)
+server :: Flag "c" '["config_file"] "String" "specify config file" (Def "config.json" String)
       -> Cmd "simple web server" ()
-server port = liftIO $ do
-  soc <- serveSocket (get port)
-  listen soc 5
-  acceptLoop soc `finally` close soc
+server c = liftIO $ do
+  conf_file <- readFile (get c)
+  case decode (BS.pack conf_file) :: Maybe Config of
+    Nothing -> print "Parse error"
+    Just conf -> do
+      soc <- serveSocket (port conf)
+      listen soc 5
+      acceptLoop soc `finally` close soc
 
